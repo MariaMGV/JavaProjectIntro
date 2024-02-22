2c2db0b5e8f104d7c0370947754ea22991b0e49d
MariaMGV <mimutu.vasileva@gmail.com>
2024-02-22 13:30:30 +0200

    Initial commit

---
+++ src/main/java/intro/MyFirstClass.java
@@ -0,0 +1,18 @@
+package intro;
+
+public class MyFirstClass {
+    //static method
+    public static void printHello(){
+        System.out.println("Hello, world!");
+    }
+    //Main method = entry point in any class (can be executed)
+    public static void main(String[] args){
+        printHello();
+
+    }
+
+
+
+    }
+}
+
